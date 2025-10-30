5282d6f25e84e5ff26e09d994743dbc27224b2c1
Kartheek Dabbiru <dabbiru.k@northeastern.edu>
2025-10-30 18:24:56 -0400

    Logs

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,4 +1,4 @@
-import { google } from '@ai-sdk/google';
+import { google, GoogleGenerativeAIProviderMetadata } from '@ai-sdk/google';
 import { streamText, UIMessage, convertToModelMessages } from 'ai';
 
 // Allow streaming responses up to 30 seconds
@@ -9,8 +9,21 @@ export async function POST(req: Request) {
 
   const result = streamText({
     model: google('gemini-2.5-flash'),
+    tools: {
+        google_search: google.tools.googleSearch({}),
+      },
     messages: convertToModelMessages(messages),
   });
+
+
+  const providerMetadata = await result.providerMetadata;
+
+  const metadata = providerMetadata?.google as
+  | GoogleGenerativeAIProviderMetadata
+  | undefined;
+  const groundingMetadata = metadata?.groundingMetadata;
+
+  console.log('groundingMetadata', JSON.stringify(groundingMetadata, null, 2));
 
   return result.toUIMessageStreamResponse();
 }
